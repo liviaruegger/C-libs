cb6ea4e76361ed80ec75efb067c40c38ed25c874
liviaruegger <analiviaruegger@gmail.com>
2023-12-07 09:16:04 -0300

    chore: criando script de compilação

---
+++ compila.sh
@@ -0,0 +1,16 @@
+# Compilar a biblioteca estática 'hashliza' (.a)
+gcc -c -o hashliza.o hashliza.c || exit 1
+ar -rcv libhashliza.a hashliza.o > /dev/null || exit 1
+
+# Compilar a biblioteca dinâmica 'shannon' (.so)
+gcc -c -fPIC -o shannon.o shannon.c || exit 1
+gcc -o libshannon.so -shared shannon.o -lm || exit 1
+
+# Compilar o programa de testes "testa.c"
+gcc testa.c -o testa -L${PWD} -lshannon -lhashliza || exit 1
+
+# Gerar documentação com doxygen
+# TODO
+
+# Tudo certo
+exit 0
